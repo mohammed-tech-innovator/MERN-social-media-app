282b24f7eb364c4072c9d99333a16649949c0f5e
Mohammed Yousif <mohammedyahyayosif@gmail.com>
2023-10-28 20:35:00 +0400

    new email

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,6 +7,13 @@ try {
     if (!token){
         return res.status(403).send("Access Denied");
     }
+    if (token.startsWith("Bearer ")){
+        token = token.slice(7, tokrn.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
 
 } catch (err){
     res.status(500).json({error : err.message})
